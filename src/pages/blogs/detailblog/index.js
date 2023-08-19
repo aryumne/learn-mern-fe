e8319f26ce3024b491f5dac7db56a3bd7448d528
Aryum NE <88039180+aryumne@users.noreply.github.com>
2023-08-19 12:39:09 +0700

    fixing code remove variable that we never used

--- src/pages/blogs/detailblog/index.js
+++ src/pages/blogs/detailblog/index.js
@@ -1,5 +1,5 @@
 import { Typography, Box } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import BoxSection from '../../../component/BoxSection';
 import Blogs from "../../../configs/dummies/Blogs";
